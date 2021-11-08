4a19bbd0f12e3f395e8bf30ead0096ec964646db
Jaromir Hamala <jaromir.hamala@gmail.com>
2021-11-08 15:31:54 +0100

    webhook listener added

---
+++ hook-listener/go.mod
@@ -0,0 +1,3 @@
+module github.com/jerrinot/turbine-demo/hook-listener
+
+go 1.16
---
+++ hook-listener/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func headers(w http.ResponseWriter, req *http.Request) {
+    for name, headers := range req.Header {
+        for _, h := range headers {
+            fmt.Fprintf(w, "%v: %v\n", name, h)
+            fmt.Printf( "%v: %v\n", name, h)
+        }
+    }
+}
+
+func main() {
+    http.HandleFunc("/hello", headers)
+    http.HandleFunc("/headers", headers)
+
+    http.ListenAndServe(":8080", nil)
+}
