3c6761772ab39384d445be413433e595420b07c1
Jaromir Hamala <jaromir.hamala@gmail.com>
2021-11-09 16:28:38 +0100

    pong to listen on port 3000

--- pong/main.go
+++ pong/main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/turbine/config", turbineConfigHandler)
-    http.HandleFunc("/ping", pingHandler)
-    log.Println("pong-service available at localhost:3001")
-    log.Fatal(http.ListenAndServe(":3001", nil))
+	http.HandleFunc("/turbine/config", turbineConfigHandler)
+	http.HandleFunc("/ping", pingHandler)
+	log.Println("pong-service available at localhost:3001")
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
 
 func turbineConfigHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Add("Content-Type", "application/json")
-    w.WriteHeader(http.StatusOK)
-    w.Write([]byte(fmt.Sprintln(`{ "serviceName": "pong-service" }`)))
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintln(`{ "serviceName": "pong-service" }`)))
 }
 
 func pingHandler(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusOK)
-    w.Write([]byte("PONG!\n"))
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("PONG!\n"))
 }
